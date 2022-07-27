b20377ac1a4a6d8552eb90f9da8c4391b00be724
lifesheets <lifesheets@outlook.com>
2022-07-27 10:15:35 +0300

    Добавлена локация и скорость транспорта.

--- client_packages/game_resources/handlers/index.js
+++ client_packages/game_resources/handlers/index.js
@@ -2,3 +2,4 @@ global.patch = 'game_resources/handlers/'
 
 require(patch + 'clothes');
 require(patch + 'vspawner');
+require(patch + 'players');
---
+++ client_packages/game_resources/handlers/players/index.js
@@ -0,0 +1,1 @@
+require(patch + 'players/location');
